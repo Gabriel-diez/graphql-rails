009fe6a444d8681e128b8e0071808e9e8494ea53
Gabriel Diez <diez.gabriel64@gmail.com>
2020-07-12 16:21:31 +0200

    add cors

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,14 @@ module GraphqlDemo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    if Rails.env.development?
+      config.middleware.insert_before 0, Rack::Cors do
+        allow do
+          origins '*'
+          resource '*', :headers => :any, :methods => [:get, :post, :options]
+        end
+      end
+    end
   end
 end
